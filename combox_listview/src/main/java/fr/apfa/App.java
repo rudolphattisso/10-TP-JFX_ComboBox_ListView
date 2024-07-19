dfc9a3f21d97f91fe7183aa557678c2bfb03e843
rudolphattisso <168489419+rudolphattisso@users.noreply.github.com>
2024-07-19 13:26:39 +0200

    Le logiciel est totalemnt fonctionnel, revoir l'interface graphique

--- combox_listview/src/main/java/fr/apfa/App.java
+++ combox_listview/src/main/java/fr/apfa/App.java
@@ -11,13 +11,13 @@ import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
-import javafx.geometry.Orientation;
+
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-
+import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 
@@ -38,80 +38,140 @@ public class App extends Application {
     public void start(Stage stage) {
 
         stage.setTitle("Country List");
+        // stage.getIcons().add(new Image("https://github.com/rudolphattisso/10-TP-jfx_ComboBox_ListView/blob/main/combox_listview/src/main/resources/icons.png"));
 
         // section1
         Label contriesLabel = new Label("Aviables contries");
         ObservableList<Country> comboBoxList = FXCollections.observableArrayList();
-       
+
         List<Country> countries = Country.getContriesList();
 
         comboBoxList.addAll(countries);
         ComboBox<Country> comboBoxCountries = new ComboBox<>(comboBoxList);
-        // comboBoxList.addListener(new ListChangeListener<Country>() { // ajout du
-        // listenner
-        // @Override
-        // public void onChanged(Change<? extends Country> country) {
 
-        // logger.info("Il s'est passé un truc, là.");
-        // }
-        // });
-
-        // section 2 haute
         Button addButton = new Button(">");
-        // if (comboBoxCountries == null) {
-        // addButton.setDisable(true);
-        // }else
-        // addButton.setDisable(true);
-
         // addButton.setDisable(true);
 
         Button addAllButton = new Button(">>");
         // addAllButton.setDisable(true);
 
         // section 2 basse
-        Button deleteButton = new Button("<");
-        Button deleteAllButton = new Button("<<");
+        Button getButton = new Button("<");
+        getButton.setDisable(true);
+        Button getAllButton = new Button("<<");
+        getAllButton.setDisable(true);
 
         // section 3
         Button upButton = new Button("Up");
         Button downButton = new Button("Down");
-        ListView listViewCountries = new ListView<Country>();
+        ListView<Country> listViewCountries = new ListView<>();
 
         Button quitButton = new Button("Quit");
 
-        // addButton.textProperty().addListener((observable, oldValue, newValue) -> {// lors de clique checkbox,la
-        //     // section Backround s'affiche
-        //     addButton.setDisable(false);
-        // });
+        listViewCountries.getItems().addListener(new ListChangeListener<Country>() {
+            @Override
+            public void onChanged(Change<? extends Country> country) {
 
+                if (comboBoxList.isEmpty()) {
+                    addButton.setDisable(true);
+                    addAllButton.setDisable(true);
+                } else {
+                    addButton.setDisable(false);
+                    addAllButton.setDisable(false);
+                }
+            }
+
+        });
+
+        comboBoxList.addListener(new ListChangeListener<Country>() {
+            @Override
+            public void onChanged(Change<? extends Country> country) {
+                if (listViewCountries.getItems().isEmpty()) {
+                    getButton.setDisable(true);
+                    getAllButton.setDisable(true);
+                    // logger.info("Il s'est passé un truc, là.");
+                } else {
+                    getButton.setDisable(false);
+                    getAllButton.setDisable(false);
+                }
+            }
+
+        });
+
+        // action
         addButton.setOnAction(e -> {
-            listViewCountries.getItems().add(comboBoxCountries.getValue());
-            comboBoxCountries.getItems().remove(comboBoxCountries.getValue());
+            // Stocker la valeur dans une variable
+            Country countryToMove = comboBoxCountries.getValue();
+            // suppression dans la comboBox
+            comboBoxCountries.getItems().remove(countryToMove);// exprission de base
+                                                               // comboBoxCountries.getItems().remove(comboBoxCountries.getValue());
+            // copie dans list view.
+            listViewCountries.getItems().add(countryToMove);
         });
 
         addAllButton.setOnAction(e2 -> {
             listViewCountries.getItems().addAll(comboBoxCountries.getItems());
             comboBoxCountries.getItems().clear();
 
-
-            
-
-        deleteAllButton.setOnAction(e ->{
-            comboBoxCountries.getItems().addAll(listViewCountries.getItems());
         });
 
-        listViewCountries.getItems().clear();
+        getButton.setOnAction(e3 -> {
+            Country countryToMoveGet = listViewCountries.getSelectionModel().getSelectedItem();
+            listViewCountries.getItems().remove(countryToMoveGet);
+            comboBoxCountries.getItems().add(countryToMoveGet);
+        });
+
+        getAllButton.setOnAction(e -> {
+            comboBoxCountries.getItems().addAll(listViewCountries.getItems());
+            listViewCountries.getItems().clear();
         });
         
-
-                quitButton.setOnAction(value -> {
+        upButton.setOnAction(e -> {
+            // recuperer l'élement selectionné et l'inserer à l'index -1
+            ObservableList<Country> list = listViewCountries.getItems();// prendre l'élément de la liste
+            // recup element selectionné
+            Country country = listViewCountries.getSelectionModel().getSelectedItem();
+            // chercher son index
+            int indexCountry = list.indexOf(country);
+            if(indexCountry != 0){
+                // supprimer ancien élement à l'index
+                list.remove(indexCountry);
+                // modif index recupéré:
+                indexCountry--;
+                // inser obj dans list avec son index.
+                list.add(indexCountry, country);
+                listViewCountries.getSelectionModel().select(indexCountry);
+            }
+        });
+        
+        downButton.setOnAction(e -> {
+            // recuperer l'élement selectionné et l'inserer à l'index -1
+            ObservableList<Country> list = listViewCountries.getItems();// prendre l'élément de la liste
+            // recup element selectionné
+            Country country = listViewCountries.getSelectionModel().getSelectedItem();
+            // chercher son index
+            int indexCountry = list.indexOf(country);
+            if(indexCountry<list.size()-1){
+                
+                
+                // supprimer ancien élement à l'index
+                list.remove(indexCountry);
+                // modif index recupéré:
+                indexCountry++;
+                // inser obj dans list avec son index.
+                list.add(indexCountry, country);
+                listViewCountries.getSelectionModel().select(indexCountry);
+            };
+        });
+        
+        quitButton.setOnAction(value -> {
             Platform.exit();
         });
 
         HBox hboxForButton = new HBox(upButton, downButton);
         VBox section1VBox = new VBox(contriesLabel, comboBoxCountries);
         VBox section2HVBox = new VBox(addButton, addAllButton);
-        VBox section2BVBox = new VBox(deleteButton, deleteAllButton);
+        VBox section2BVBox = new VBox(getButton, getAllButton);
         VBox section3VBox = new VBox(hboxForButton, listViewCountries, quitButton);
 
         GridPane gridPane = new GridPane();
